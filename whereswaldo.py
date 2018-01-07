8350a66db9d40cbe0d79743841a131a02329f110
Austin Bailey <baileyau@bc.edu>
2018-01-07 09:39:01 -0500

    found key points on train image

--- whereswaldo.py
+++ whereswaldo.py
@@ -1,8 +1,21 @@
 import cv2 as cv
+from matplotlib import pyplot as plt
 
 print "---Where's Waldo---"
 
+#feature detection
+
+## read in image
 waldo = cv.imread('./waldo.jpg')
 
-cv.imwrite('./newWaldo.jpg', waldo)
+## create ORB object, detect and compute key points on waldo image
+orb = cv.ORB_create()
+kp = orb.detect(waldo, None)
+kp, des = orb.compute(waldo, kp)
+
+## draw the key points of waldo on 'img'
+img = cv.drawKeypoints(waldo, kp, None)
+
+## show 'img'
+cv.imshow('image', img)
 
